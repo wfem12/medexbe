1962214dadbff5a61bd8c1074c9c892828d2f5e6
Fernando <wfem12@gmail.com>
2022-03-14 14:38:04 -0600

    REACT

--- medexfe/src/App.js
+++ medexfe/src/App.js
@@ -1,36 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
-import {Title} from './Components/UX/Title';
+
+import Splash from './Components/Views/Splash';
+import Signin from './Components/Views/SigninPage';
 
 function App() {
   return (
-    <div>
-      <Title>Hola Mundo Cruel</Title>
-      <Title>Hola Mundo Cruel1</Title>
-      <Title>Hola Mundo Cruel2</Title>
-      <Title>Hola Mundo Cruel3</Title>
-      <Title>
-        Hola Mundo Cruel4
-        <Title>Hola Mundo Cruel5</Title>
-      </Title>
-    </div>
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
+    <Signin></Signin>
   );
 }
+
 
 export default App;
--- medexfe/src/Components/UX/Title.js
+++ medexfe/src/Components/UX/Title.js
@@ -1,5 +1,6 @@
-export const Title = ({children}) =>{
-    return(
-        <h1>{children}</h1>
-    )
-}
+export const Title = ({children})=>{
+    return (
+      <h1>{children}</h1>
+    );
+  }
+  
