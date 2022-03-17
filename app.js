4c7eaaea59a53fbad2a1dd5451dd6aac0c62697c
Fernando <wfem12@gmail.com>
2022-03-16 18:21:10 -0600

    TODO FUNCIONAL DESDE AQUI

--- app.js
+++ app.js
@@ -3,12 +3,28 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+var cors = require('cors'); //CORS
+var whiteList = (process.env.CORS_ORIGIN || 'http://localhost:3001').split(','); //CORS
+
+//APLICAR LOS CORS
+var corsOptions ={
+    origin: (origin, callback) =>{
+        if (whiteList.indexOf(origin) >= 0) {
+            callback(null, true);
+        } else {
+            callback(new Error('CORS not allowed'));
+        }
+    }
+}
+//_________________________________________________
+
 var indexRouter = require('./routes/index');
 var apiRouter = require('./routes/api/api');
 
 var app = express();
 
 app.use(logger('dev'));
+app.use(cors(corsOptions));// SE AGREGA AQUI
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
